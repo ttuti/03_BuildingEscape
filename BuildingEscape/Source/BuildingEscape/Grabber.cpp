d3622d5bbe43ea96b460043d16074b85ce7856e4
Tutiworks <timothy_tuti@hotmail.com>
2017-12-11 16:46:27 +0000

    Initial commit after crash

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -1,32 +1,31 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "PostionReport.h"
-#include "GameFramework/Actor.h"
+#include "Grabber.h"
 
 
 // Sets default values for this component's properties
-UPostionReport::UPostionReport()
+UGrabber::UGrabber()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-	
+
+	// ...
 }
 
 
 // Called when the game starts
-void UPostionReport::BeginPlay()
+void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
-	FString ObjectName = GetOwner()->GetName();
-	FString ObjectPos = GetOwner()->GetActorLocation().ToString();
-	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *ObjectName, *ObjectPos);
 
+	// ...
+	
 }
 
 
 // Called every frame
-void UPostionReport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
---
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "OpenDoor.h"
+#include "Engine/World.h"
+#include "GameFramework/Actor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay(){
+	Super::BeginPlay();
+	Owner = GetOwner();
+	
+	ActorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
+}
+
+void UOpenDoor::OpenDoor(){
+	Owner->SetActorRotation(FRotator(0.0f, OpenAngle, 0.0f)); 
+}
+
+void UOpenDoor::CloseDoor(){
+	Owner->SetActorRotation(FRotator(0.f, 0.f, 0.f)); 
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	//Poll the trigger volume
+	if(PressurePlate->IsOverlappingActor(ActorThatOpens)) {
+		OpenDoor();
+		LastDoorOpenTime = GetWorld()->GetRealTimeSeconds();
+	}	
+	
+	//Check if its time to close the door
+	if (GetWorld()->GetRealTimeSeconds() - LastDoorOpenTime > DoorCloseDelay) {
+		CloseDoor();
+	}
+}
+
